c7225031d01802a5517c8b93154cdee3da5f8321
julia <juliaduran1515@gmail.com>
2023-12-05 09:47:39 -0300

    Ultimas alteracoes

--- src/components/common/ModalRecycle/index.jsx
+++ src/components/common/ModalRecycle/index.jsx
@@ -2,6 +2,7 @@ import React,{useState} from "react";
 import {Modal,Button,Space} from "antd";
 import "./index.scss";
 import { AiOutlinePicture } from "react-icons/ai";
+import { Link } from 'react-router-dom';
 
 
 
@@ -11,6 +12,7 @@ const ModalRecycleComponent = ()=>{
   const [modal2Open, setModal2Open] = useState(false);
   const [modal3Open, setModal3Open] = useState(false);
   const [modal4Open, setModal4Open] = useState(false);
+  const [modal5Open, setModal5Open] = useState(false);
     
   
     
@@ -144,14 +146,37 @@ const ModalRecycleComponent = ()=>{
        <div className="info-details">
        <h1 className="About2-recicle"> O que NÃO pode ser descartado sem cuidados proprios:  </h1>
 
-       <li className="about2-extra-info">Pilhas e baterias: Esses dispositivos muitas vezes contêm metais pesados, como mercúrio, chumbo e cádmio, que são prejudiciais ao meio ambiente. Pilhas e baterias devem ser descartadas em pontos de coleta específicos para reciclagem.</li>
-       <li className="about2-extra-info">Produtos eletrônicos: Equipamentos eletrônicos, como computadores, telefones celulares e televisores, frequentemente contêm substâncias tóxicas, incluindo metais pesados e produtos químicos. Eles devem ser encaminhados para reciclagem adequada.</li>
-       <li className="about2-extra-info">Medicamentos vencidos: Não devem ser jogados no lixo comum ou no esgoto, pois podem contaminar a água. Muitas farmácias e postos de saúde oferecem locais para devolução segura de medicamentos.</li>
-       <li className="about2-extra-info">Óleos e gorduras: Não devem ser despejados na pia, pois podem entupir encanamentos e causar poluição da água. O descarte adequado envolve a coleta em recipientes apropriados para reciclagem.</li>
-       <li className="about2-extra-info">Pneus: Pneus descartados de forma inadequada podem ser criadouros de mosquitos e causar impactos ambientais. Muitos centros de reciclagem aceitam pneus para processamento adequado.</li>
-       <li className="about2-extra-info">Agulhas e seringas: Devem ser descartadas em recipientes próprios, conhecidos como "pontocentros" ou "pontos de coleta". Isso evita riscos de acidentes e contaminação.</li>
+       <li className="about2-extra-info"><strong className="about2-extra-info-strong">Pilhas e baterias</strong>: Esses dispositivos muitas vezes contêm metais pesados, como mercúrio, chumbo e cádmio, que são prejudiciais ao meio ambiente. Pilhas e baterias devem ser descartadas em pontos de coleta específicos para reciclagem.</li>
+       <li className="about2-extra-info"><strong className="about2-extra-info-strong">Produtos eletrônicos</strong>: Equipamentos eletrônicos, como computadores, telefones celulares e televisores, frequentemente contêm substâncias tóxicas, incluindo metais pesados e produtos químicos. Eles devem ser encaminhados para reciclagem adequada.</li>
+       <li className="about2-extra-info"><strong className="about2-extra-info-strong">Medicamentos vencidos</strong>: Não devem ser jogados no lixo comum ou no esgoto, pois podem contaminar a água. Muitas farmácias e postos de saúde oferecem locais para devolução segura de medicamentos.</li>
+       <li className="about2-extra-info"><strong className="about2-extra-info-strong">Óleos e gorduras</strong>: Não devem ser despejados na pia, pois podem entupir encanamentos e causar poluição da água. O descarte adequado envolve a coleta em recipientes apropriados para reciclagem.</li>
+       <li className="about2-extra-info"><strong className="about2-extra-info-strong">Pneus</strong>: Pneus descartados de forma inadequada podem ser criadouros de mosquitos e causar impactos ambientais. Muitos centros de reciclagem aceitam pneus para processamento adequado.</li>
+       <li className="about2-extra-info"><strong className="about2-extra-info-strong">Agulhas e seringas</strong>: Devem ser descartadas em recipientes próprios, conhecidos como "pontocentros" ou "pontos de coleta". Isso evita riscos de acidentes e contaminação.</li>
 
-       </div>
+       
+       <button className="common-btn2"type="primary" onClick={() => setModal5Open(true)}>
+        Não sabe onde reciclar? Clique AQUI!
+      </button>
+        <Modal
+          title="Sobre DESCARTE PROPRIO!"
+          centered
+          open={modal5Open}
+          onOk={() => {
+            setModal5Open(false);
+        }}
+          onCancel={() => setModal5Open(false)}
+          >
+
+  
+        <div className="info-modal">
+         Existem varios tipos de descarte!
+         Saiba onde estão os postos de reciclagem e de descarte próprio perto de você!
+          
+         < Link to="https://www.ecycle.com.br/postos/reciclagem.php" className="common-btn3" >Procure Aqui!</Link>
+        
+        </div>
+        </Modal>
+        </div>
        </div>
         
       </> 
